4387c0db55c24598d1d66987210caa6dd041cfb1
Alex <irimida@i.ua>
2019-07-09 11:04:11 +0300

    services ready

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(function(){
 
-    $('.slider__inner').slick();
+    $('.slider__inner').slick({
+        prevArrow : '<button type="button" class="slick-btn slick-prev"></button>',
+        nextArrow : '<button type="button" class="slick-btn slick-next"></button>',
+        infinite: false
+    });
 
 
 
